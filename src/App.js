66f4f74129c5059d8cc3accd3f3538d6cf32e8f7
Nick Guerra <nickg@maliciousgames.com>
2020-09-01 19:39:42 -0500

    added form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello react</h1>
+      <header>
+        <h1>Nick's Todo List</h1>
+      </header>
+      <Form />
     </div>
   );
 }
