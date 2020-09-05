1fc56bfab749bbd6baff146bbe0a72cf86bd68ba
Nick Guerra <nickg@maliciousgames.com>
2020-09-04 19:04:11 -0500

    added filters

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import Todo from "./Todo";
+import Todo from './Todo';
 
-const TodoList = ({ todos }) => {
-
-    return (
-        <div className="todo-container">
-            <ul className="todo-list">
-                {todos.map(todo => (
-                    <Todo key={todo.id} text={todo.text} />
-                ))}
-            </ul>
-        </div>
-    );
+const TodoList = ({todos, setTodos, filteredTodos}) => {
+  return (
+    <div className='todo-container'>
+      <ul className='todo-list'>
+        {filteredTodos.map((todo) => (
+          <Todo key={todo.id} text={todo.text} setTodos={setTodos} todo={todo} todos={todos} />
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default TodoList;
